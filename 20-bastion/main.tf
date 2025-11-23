96dae49e32ac00ba7b79c76f1bb0c7e23bf0507f
Satish <sat116@gmail.com>
2025-11-23 18:07:12 -0500

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,12 @@ resource "aws_instance" "bastion" {
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id # Assuming the first public subnet is used.. derived in locals
 
+    # need more for terraform
+    root_block_device {
+      volume_size = 50
+      volume_type = "gp3"   # or "gp2", depending on your performance
+    }
+
     tags = merge(
         local.common_tags,
         {           
