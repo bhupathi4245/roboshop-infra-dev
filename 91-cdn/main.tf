7d6c5ca1fa63fad5b5782f29bfb8bf9abd42f364
Satish <sat116@gmail.com>
2026-03-31 22:20:15 -0400

    roboshop-infra-dev

--- 91-cdn/main.tf
+++ 91-cdn/main.tf
@@ -58,7 +58,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "cdn.${var.zone_name}" #dev.daws84s.site
+  name    = "cdn.${var.zone_name}" #dev.akinfotech.site
   type    = "A"
 
   alias {
